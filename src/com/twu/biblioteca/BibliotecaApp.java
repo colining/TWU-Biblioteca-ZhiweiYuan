f6e534fa3a5533862c964f45cb6510b07923698f
Chiwai <chiwaireg@hotmail.com>
2018-07-12 22:26:30 +0800

    implement Welcome Message/List Books function in console

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,8 +1,14 @@
 package com.twu.biblioteca;
 
+import java.io.PrintStream;
+
 public class BibliotecaApp {
 
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        PrintStream printStream = System.out;
+        new Welcome(printStream).welcome();
+        BookList bookList = new BookList();
+        bookList.init();
+        printStream.print(bookList);
     }
 }
