f2ce78dd90766cb88f74e00bbab7e4e7fd5a39d6
Chiwai <chiwaireg@hotmail.com>
2018-07-13 00:53:10 +0800

    Refactor the biblioteca

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,14 +1,39 @@
 package com.twu.biblioteca;
 
+import com.sun.tools.javac.Main;
+
 import java.io.PrintStream;
+import java.util.Scanner;
 
 public class BibliotecaApp {
+    private PrintStream printStream;
+    private Scanner input;
+    private Welcome welcome;
+    private BookList bookList;
+    private MainMenu mainMenu;
+
+    public BibliotecaApp(PrintStream printStream, Scanner input, Welcome welcome, BookList bookList, MainMenu mainMenu) {
+        this.printStream = printStream;
+        this.input = input;
+        this.welcome = welcome;
+        this.bookList = bookList;
+        this.mainMenu = mainMenu;
+    }
+
+    BibliotecaApp() {
+        this.printStream = System.out;
+        this.input = new Scanner(System.in);
+        this.welcome = new Welcome(this.printStream);
+        this.bookList = new BookList();
+        this.bookList.init();
+        this.mainMenu = new MainMenu(this.printStream);
+
+    }
 
     public static void main(String[] args) {
-        PrintStream printStream = System.out;
-        new Welcome(printStream).welcome();
-        BookList bookList = new BookList();
-        bookList.init();
-        printStream.print(bookList);
+        BibliotecaApp app = new BibliotecaApp();
+        app.welcome.welcome();
+        app.printStream.print(app.mainMenu);
+
     }
 }
