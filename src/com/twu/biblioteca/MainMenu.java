2dc79cc2e01ed20bf39b6dd9890ef53227c472d3
Chiwai <chiwaireg@hotmail.com>
2018-07-12 23:10:57 +0800

    add invalid option

--- src/com/twu/biblioteca/MainMenu.java
+++ src/com/twu/biblioteca/MainMenu.java
@@ -1,19 +1,36 @@
 package com.twu.biblioteca;
 
+import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class MainMenu {
     private List<String> menu;
+    private PrintStream printStream;
 
-    MainMenu() {
+    private final static String INVALID_OPTION;
+
+    static {
+        INVALID_OPTION = "Select a valid option!";
+    }
+
+    MainMenu(PrintStream printStream) {
         menu = new ArrayList<>();
         menu.add("1. List Books");
+        this.printStream = printStream;
     }
 
     @Override
     public String toString() {
         return menu.stream().collect(Collectors.joining("\n"));
     }
+
+    void choose(int number) {
+        if (number > menu.size()) {
+            printStream.print(INVALID_OPTION);
+        }
+    }
+
+
 }
