a24066b4e264c54c629b709532ed273f7112325a
Chiwai <chiwaireg@hotmail.com>
2018-07-12 22:10:24 +0800

    add BookList Test

---
+++ test/com/twu/biblioteca/BookListTest.java
@@ -0,0 +1,23 @@
+package com.twu.biblioteca;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class BookListTest {
+    private BookList bookList;
+    private String expectedList;
+
+
+    @Test
+    public void testShowBooks() {
+        assertEquals(expectedList,bookList.toString());
+    }
+
+    @Before
+    public void setUp()  {
+        bookList.init();
+        expectedList = String.format("%-15s %-15s %-30s %n", "Test-driven Development: By Example", "Kent Beck", 2003);
+    }
+}
